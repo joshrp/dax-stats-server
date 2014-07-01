7de2798186bba134dabfdf0dea3c2da34f9104ac
Josh Priestley <josh.priestley@bbc.co.uk>
2014-07-01 16:28:47 +0100

    Initial server to pull DAX stats and server them statically later on.

---
+++ config.js
@@ -0,0 +1,27 @@
+baseConfig = {
+	baseDaxUrl: 'https://sitestatapi.com/v1/reportitems.xml?nrofrows=1000&itemid={itemId}&site={site}&startdate={startDate}&enddate={endDate}&client=bbc&user={username}&password={password}&format=json&parameters={params}',
+	password: '',
+	username: '',
+	site: ''
+}
+args = process.argv.slice(2);
+
+baseConfig.username = process.env.DAX_USERNAME;
+if (!baseConfig.username) {
+	console.error('No DAX username given. Set $DAX_USERNAME');
+	process.exit(1)
+}
+
+baseConfig.password = process.env.DAX_PASSWORD;
+if (!baseConfig.password) {
+	console.error('No DAX password given. Set $DAX_PASSWORD');
+	process.exit(1)
+}
+
+baseConfig.site = process.env.DAX_SITE;
+if (!baseConfig.site) {
+	console.error('No DAX site given. Set $DAX_SITE');
+	process.exit(1)
+}
+
+module.exports = baseConfig;
---
+++ server.js
@@ -0,0 +1,69 @@
+var request = require('request'),
+		restify = require('restify'),
+		server = restify.createServer(),
+		stats = {},
+		feeds = require('./feeds'),
+		config = require('./config')
+
+
+var buildDaxUrl = function (feed) {
+	var baseUrl = config.baseDaxUrl;
+	var replacements = {
+		itemId: feed.id,
+		params: [],
+		site: config.site,
+		password: config.password,
+		username: config.username,
+		startDate: 'today',
+		endDate: 'today'
+	}
+	Object.keys(feed.params).forEach(function (k) {
+		replacements.params.push(k + ':' + feed.params[k]);
+	})
+	replacements.params = replacements.params.join('|');
+
+	Object.keys(replacements).forEach(function (k) {
+		baseUrl = baseUrl.replace('{' + k + '}', replacements[k]);
+	});
+
+	return baseUrl;
+}
+
+feeds.forEach(function (feed) {
+	stats[feed.name] = false;
+	console.log('fetching:',buildDaxUrl(feed))
+	request(buildDaxUrl(feed), function (err, resp, body) {
+		if (err) {
+		 	throw err;
+		}
+		if (resp.statusCode !== 200) {
+			err = JSON.parse(body);
+			msg = ''
+			if (err && err.ERROR)
+				msg = err.ERROR;
+			throw new Error('Error fetching data. Code: '+resp.statusCode+'. Message: '+msg);
+		}
+		stats[feed.name] = JSON.parse(body).reportitems.reportitem[0].rows.r;
+	});
+})
+
+server.use(restify.CORS({
+    credentials: true
+}));
+
+server.get('/stats/:name', function (req, res, next) {
+	var name = req.params.name;
+
+	if (!(name in stats)) {
+		res.send(404);
+	} else if (stats[name] === false) {
+		res.send(204);
+	} else {
+		res.json(stats[name]);
+	}
+});
+
+server.listen(9615, function() {
+  console.log('%s listening at %s', server.name, server.url);
+});
+
