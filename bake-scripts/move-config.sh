7b3407ed12724f29fea286a8ab0803033845f14c
Josh Priestley <josh.priestley@bbc.co.uk>
2014-08-20 18:33:50 +0100

    Remove uneeded sudo

--- bake-scripts/move-config.sh
+++ bake-scripts/move-config.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 dest=/usr/lib/iplayer-dax-stats/configuration.json
-sudo cp -r /etc/iplayer-dax-stats/component_configuration.json $dest
-sudo chmod 777 $dest
+cp -r /etc/iplayer-dax-stats/component_configuration.json $dest
+chmod 777 $dest
