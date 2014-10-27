3d38647c842a3527f21556318f740f41090103ed
Josh Priestley <josh.priestley@bbc.co.uk>
2014-10-27 16:58:53 +0000

    Adding new response type for stats calls. No Data may be available for that feed for that day so we should return 204. Also changed the response code for stats that haven't been fetched yet to a 202 which is more correct, and differentiates it from no data available

--- src/index.js
+++ src/index.js
@@ -4,12 +4,12 @@ var request = require('request'),
 	stats = {},
 	config = require('config'),
 	Feeds = require('./Feeds'),
-	startDate, endDate,
+	currentStatsDate,
 	moment = require('moment')
 
-startDate = endDate = moment('2014-10-21');
+currentStatsDate = moment('2014-10-28');
 
-feeds = new Feeds(startDate)
+feeds = new Feeds(currentStatsDate)
 
 feeds.getAll().forEach(function (feed) {
 	stats[feed.name] = false;
@@ -17,17 +17,21 @@ feeds.getAll().forEach(function (feed) {
 	var timeStart = new Date().getTime();
 
 	request(feed.url, function (err, resp, body) {
+		var msg, respErr;
 		if (err) {
 		 	throw err;
 		}
-		if (resp.statusCode !== 200) {
-			err = JSON.parse(body);
+		respErr = JSON.parse(body);
+		if (resp.statusCode === 400 && respErr && respErr.ERROR && respErr.ERROR === 'No data') {
+			stats[feed.name] = [];
+		} else if (resp.statusCode !== 200)  {
 			msg = ''
-			if (err && err.ERROR)
-				msg = err.ERROR;
+			if (respErr && respErr.ERROR)
+				msg = respErr.ERROR;
 			throw new Error('Error fetching data for ' + feed.name + ' using ' + feed.url  + '. Code: '+resp.statusCode+'. Message: '+msg);
+		} else {
+			stats[feed.name] = JSON.parse(body).reportitems.reportitem[0].rows.r;
 		}
-		stats[feed.name] = JSON.parse(body).reportitems.reportitem[0].rows.r;
 		var timeEnd = new Date().getTime();
 		console.log('Fetched',feed.name, 'in', timeEnd-timeStart+'ms')
 	});
@@ -39,15 +43,19 @@ server.use(restify.CORS({
 
 server.get('/stats/:name', function (req, res, next) {
 	var name = req.params.name;
-
 	if (!(name in stats)) {
+		// No idea what this is
 		res.send(404);
 	} else if (stats[name] === false) {
+		// Stats not fetched yet
+		res.send(202);
+	} else if (stats[name].length === 0) {
+		// No stats available
 		res.send(204);
 	} else {
 		res.json({
 			stats: stats[name],
-			date: startDate.format('X')
+			date: currentStatsDate.format('X')
 		});
 	}
 });
@@ -57,13 +65,14 @@ server.get('/status', function (req, res) {
 
 	for (var feed in stats) {
 		filled[feed] = {
-			fecthed: stats[feed].length > 0,
+			fecthed: stats[feed] !== false,
+			hasData: stats[feed].length > 0,
 			href: 'https://' + req.headers['x-forwarded-host'] + '/stats/' + feed,
 		}
 	}
 
 	res.json({
-		date: startDate.format('X'),
+		date: currentStatsDate.format('X'),
 		feeds: filled
 	});
 })
